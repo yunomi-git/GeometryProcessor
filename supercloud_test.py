2469f5080265378899f6e9f06b6a8695746fd542
Nomi <yunomi@mit.edu>
2024-01-24 16:31:43 -0500

    add stopwatch to supercloud

--- supercloud_test.py
+++ supercloud_test.py
@@ -11,11 +11,11 @@ def save(points, values, filename):
     names = ["x", "y", "z", "value"]
     data = np.concatenate([points, values[:, np.newaxis]], axis=1)
     df = pd.DataFrame(data=data, columns=names)
-    print("saving")
-    print(filename)
+    # print("saving")
+    # print(filename)
     df.to_csv(filename + ".csv", index=False, header=True)
 
-total_stls = 20
+total_stls = 10000
 my_task_id = 0
 num_tasks = 1
 
@@ -24,7 +24,10 @@ if __name__ == "__main__":
     my_task_id = int(sys.argv[2])
     num_tasks = int(sys.argv[3])
 
+    stopwatch = Stopwatch()
+
     # ## Multi STL
+    stopwatch.start()
     for i in range(my_task_id, total_stls, num_tasks):
         # random_index = random.randint(0, 10000)
         # print(random_index)
@@ -36,5 +39,6 @@ if __name__ == "__main__":
 
         if points is not None:
             save(points, values, paths.HOME_PATH + "generation_output/TRIMESH_" + str(index))
+    stopwatch.print_time()
 
 
