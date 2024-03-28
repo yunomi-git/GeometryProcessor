e3a69a9a37c95877d26ab80287a107bf4b60d212
Nomi Yu <nomiyu134@gmail.com>
2024-03-27 23:52:18 -0400

    remeshing works with curvature

--- new_remesh.py
+++ new_remesh.py
@@ -34,46 +34,42 @@ selection_string = create_face_selection(low_curvature)
 
 
 ms = pymeshlab.MeshSet()
-ms.load_new_mesh(paths.HOME_PATH + "stls/kunai.stl")
+ms.load_new_mesh(paths.HOME_PATH + "stls/crane.stl")
 # ms.add_mesh(labmesh)
 time.start()
-# ms.compute_selection_by_condition_per_face(condselect =selection_string)
 
-# ms.compute_selection_by_condition_per_face(condselect ="(q0 < 0.01)||(q1 < 0.01)||(q2 < 0.01)")
-ms.compute_curvature_and_color_rimls_per_vertex()
-a = ms.mesh(0).face_color_matrix()
-# ms.compute_scalar_by_discrete_curvature_per_vertex(curvaturetype="Mean Curvature")
-ms.compute_selection_by_color_per_face(color=pymeshlab.Color(r=255, g=100, b=100),
-                                        percentrh=0.5,
-                                       percentgs=1.0,
-                                       percentbv=1.0)
-
-ms.meshing_isotropic_explicit_remeshing(smoothflag=True,
-                                        # adaptive=True,
-                                        iterations=3,
-                                        splitflag=True,
-                                        featuredeg=30,
-                                        checksurfdist =True,
-                                        collapseflag=False,
-                                        # swapflag=False,
-                                        # selectedonly =True,
-                                        targetlen=pymeshlab.PercentageValue(10),
-                                        # maxsurfdist=pymeshlab.PercentageValue(0.5)
-                                        )
+# First obtain minimum resolution
+ms.meshing_surface_subdivision_midpoint(threshold = pymeshlab.PercentageValue(3.000003))
 # ms.meshing_isotropic_explicit_remeshing(smoothflag=True,
 #                                         # adaptive=True,
 #                                         iterations=3,
-#                                         splitflag=False,
+#                                         splitflag=True,
 #                                         featuredeg=30,
 #                                         checksurfdist =True,
-#                                         collapseflag=True,
+#                                         collapseflag=False,
 #                                         # swapflag=False,
 #                                         # selectedonly =True,
-#                                         targetlen=pymeshlab.PercentageValue(0.1),
+#                                         targetlen=pymeshlab.PercentageValue(10),
 #                                         # maxsurfdist=pymeshlab.PercentageValue(0.5)
 #                                         )
-# ms.meshing_merge_close_vertices(threshold=pymeshlab.PercentageValue(5))
-# ms.meshing_surface_subdivision_midpoint(iterations=3,
-#                                          threshold=pymeshlab.PercentageValue(1))
+
+# At good resolution, calculate curvatures
+ms.compute_scalar_by_discrete_curvature_per_vertex(curvaturetype = 3)
+
+# Then collapse large faces
+ms.compute_selection_by_color_per_face(color = pymeshlab.Color(255, 0, 0, 255), percentrh = 0.1, percentgs = 0.090000, percentbv = 0.110000)
+ms.meshing_isotropic_explicit_remeshing(iterations = 3,
+                                        selectedonly = True,
+                                        targetlen = pymeshlab.PercentageValue(3),
+                                        splitflag=True,
+                                        collapseflag = True)
+
+# Also refine medium faces
+ms.compute_selection_by_color_per_face(color = pymeshlab.Color(0, 255, 8, 255))
+ms.meshing_isotropic_explicit_remeshing(iterations = 3,
+                                        selectedonly = True,
+                                        targetlen = pymeshlab.PercentageValue(0.5),
+                                        splitflag=True, collapseflag = True)
+
 time.print_time()
 ms.show_polyscope()
