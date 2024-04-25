visualization and fixed normalization not saving in dataset

dataset/process_and_save.py
@@ -119,14 +119,6 @@ class MeshDatasetFileManager:
         else:
             targets = targets[:, :, label_indices]
 
-        # # Convert targets to dictionary
-        # targets_dict = {}
-        # for i in range(len(label_names)):
-        #     key = label_names[i]
-        #     if key not in targets_dict:
-        #         targets_dict[key] = []
-        #     targets_dict[key] = targets[:, i]
-
         return clouds, targets
 
 # def test_load_numpy_pointclouds():
@@ -150,8 +142,13 @@ def get_augmented_mesh(mesh: trimesh.Trimesh, augmentations):
     # "scale":
     ###
     orientation = augmentations["orientation"]
-    eulers = [orientation["x"], orientation["y"], orientation["z"]]
-    scale = augmentations["scale"]
+    eulers = [orientation["x"],
+              orientation["y"],
+              orientation["z"]]
+
+    scale = np.array([augmentations["scale"]["x"],
+                      augmentations["scale"]["y"],
+                      augmentations["scale"]["z"]])
 
     transformed_mesh = trimesh_util.get_transformed_mesh_trs(mesh, scale=scale, orientation=eulers)
 
@@ -194,6 +191,8 @@ def save_base_mesh_and_target(mesh, mesh_file_manager: MeshDatasetFileManager, m
                 largest_submesh = submesh
         mesh = largest_submesh
 
+    mesh = normalize_mesh(mesh, center=center, normalize_scale=normalize_scale)
+
     target = {
         "base_name": mesh_name,
         "mesh_relative_path": mesh_file_manager.get_mesh_path(absolute=False) + mesh_name + ".stl",
@@ -207,18 +206,14 @@ def save_base_mesh_and_target(mesh, mesh_file_manager: MeshDatasetFileManager, m
         
     # Add the first instance
     augmentations = {
-        "orientation": {
-            "x": 0.0,
-            "y": 0.0,
-            "z": 0.0
-        },
-        "scale": 1.0
+        "orientation": {"x": 0.0, "y": 0.0, "z": 0.0},
+        "scale": {"x": 1.0, "y": 1.0, "z": 1.0}
     }
     mesh = get_augmented_mesh(mesh, augmentations)
     mesh = normalize_mesh(mesh, center=center, normalize_scale=normalize_scale)
-    save_mesh_and_augmentation(target=target,
-                               mesh=mesh,
-                               augmentations=augmentations)
+    save_labels_for_mesh(target=target,
+                         mesh=mesh,
+                         augmentations=augmentations)
     with open(target_path_absolute, 'w') as f:
         json.dump(target, f)
     return target, mesh
@@ -239,7 +234,7 @@ def normalize_mesh(mesh: trimesh.Trimesh, center, normalize_scale):
     mesh = trimesh_util.get_transformed_mesh_trs(mesh, scale=normalization_scale, translation=normalization_translation)
     return mesh
 
-def save_mesh_and_augmentation(mesh: trimesh.Trimesh, target: dict, augmentations: dict):
+def save_labels_for_mesh(mesh: trimesh.Trimesh, target: dict, augmentations: dict):
     instance_target = calculate_instance_target(mesh, augmentations=augmentations)
     target["instances"].append(instance_target)
 
@@ -261,11 +256,11 @@ def generate_base_dataset(data_root_dir, source_mesh_filenames, save_prefix, mes
             print("mesh skipped:", mesh_path)
             continue
 
-        # if not mesh.is_watertight:
-        #     mesh.fill_holes()
-        #     if not mesh.is_watertight:
-        #         print("mesh not watertight. skipped:", mesh_path)
-        #         continue
+        if not mesh.is_watertight:
+            mesh.fill_holes()
+            if not mesh.is_watertight:
+                print("mesh not watertight. skipped:", mesh_path)
+                continue
 
         # mesh.apply_scale(mesh_scale)
         try:
@@ -288,7 +283,7 @@ def generate_augmentations_for_dataset(mesh_file_manager: MeshDatasetFileManager
         for augmentation in augmentation_list:
             mesh = get_augmented_mesh(mesh, augmentation)
             mesh = normalize_mesh(mesh, center=center, normalize_scale=normalize_scale)
-            save_mesh_and_augmentation(mesh=mesh, target=target, augmentations=augmentation)
+            save_labels_for_mesh(mesh=mesh, target=target, augmentations=augmentation)
 
         with open(target_path, 'w') as f:
             json.dump(target, f)
@@ -305,7 +300,12 @@ def quick_edit(data_root_directory):
         with open(file, 'w') as f:
             json.dump(target, f)
 
-def save_generated_dataset_as_numpy(file_manager, max_mesh_per_file, num_points_to_sample: int, sampling_method="mixed"):
+def save_generated_dataset_as_numpy(file_manager,
+                                    max_mesh_per_file,
+                                    num_points_to_sample: int,
+                                    sampling_method="mixed",
+                                    normalize_scale=True,
+                                    center=True):
     num_points_to_sample = int(num_points_to_sample)
     max_mesh_per_file = int(max_mesh_per_file)
     Path(mesh_file_manager.get_numpy_pointcloud_path(absolute=True, sampling_method=sampling_method)).mkdir(parents=True, exist_ok=True)
@@ -328,14 +328,14 @@ def save_generated_dataset_as_numpy(file_manager, max_mesh_per_file, num_points_
     ]
 
     extra_label_names = [
-        "surface_area",
+        # "surface_area",
     ]
 
     per_point_label_names = [
         "nx",
         "ny",
         "nz",
-        "curvature",
+        # "curvature",
         "thickness"
     ]
 
@@ -346,19 +346,19 @@ def save_generated_dataset_as_numpy(file_manager, max_mesh_per_file, num_points_
 
         for instance_data in instances:
             mesh = get_augmented_mesh(mesh, instance_data)
+            mesh = normalize_mesh(mesh, normalize_scale=normalize_scale, center=center)
 
             try:
                 mesh_aux = trimesh_util.MeshAuxilliaryInfo(mesh)
                 vertices, normals, face_ids = mesh_aux.sample_and_get_normals(count=int(num_points_to_sample * 1.2), # Scaling is for thickness calculation failures
                                                                           use_weight=sampling_method, return_face_ids=True)
                 _, thicknesses, num_hits = mesh_aux.calculate_thickness_at_points(points=vertices, normals=normals)
-                _, curvature = mesh_aux.calculate_curvature_at_points(origins=vertices, face_ids=face_ids,
-                                                                      curvature_method="defect")
+                # _, curvature = mesh_aux.calculate_curvature_at_points(origins=vertices, face_ids=face_ids,
+                #                                                       curvature_method="defect")
 
             except:
                 print("sampling error")
                 continue
-            # np.random.shuffle(vertices)
 
             ## Per point labels
             # calculate thicknesses / point
@@ -369,19 +369,23 @@ def save_generated_dataset_as_numpy(file_manager, max_mesh_per_file, num_points_
             vertices = vertices[:num_points_to_sample]
             normals = normals[:num_points_to_sample]
             thicknesses = thicknesses[:num_points_to_sample]
-            curvature = curvature[:num_points_to_sample]
+            # curvature = curvature[:num_points_to_sample]
 
-            per_point_label = np.concatenate((normals, curvature[:, np.newaxis], thicknesses[:, np.newaxis]), axis=1)
+            per_point_label = np.concatenate((normals,
+                                              # curvature[:, np.newaxis],
+                                              thicknesses[:, np.newaxis]),
+                                             axis=1)
             # per_point_label = normals
+            # per_point_label = thicknesses[:, np.newaxis]
 
             assert(len(vertices) == num_points_to_sample)
 
             ## Global label
             label = np.array([instance_data[label_name] for label_name in label_names])
             # Append SA
-            label = np.concatenate((label, np.array([mesh_aux.surface_area])))
+            # label = np.concatenate((label, np.array([mesh_aux.surface_area])))
             # Append Centroid
-            label = np.concatenate((label, np.mean(vertices, axis=0)))
+            # label = np.concatenate((label, np.mean(vertices, axis=0)))
 
             ## Save
             all_point_clouds.append(vertices)
@@ -440,13 +444,13 @@ if __name__ == "__main__":
     # quick_edit(paths.HOME_PATH + "data_augmentations/")
     # 1/0
     # Generation Parameters
-    outputs_save_path = paths.DATA_PATH + "modelnet_tables/"
+    outputs_save_path = paths.DATA_PATH + "mcb_scale_a/"
 
     mode = "convert_numpy" # generate_initial, add_augmentations, both, convert_numpy
     normalize_center = True
-    normalize_scale = False
+    normalize_scale = True
     if mode == "generate_initial" or mode == "both":
-        use_dataset = "modelnet_tables" # onshape, thingiverse, custom
+        use_dataset = "custom" # onshape, thingiverse, custom
         source_mesh_filenames = []
         if use_dataset == "onshape":
             min_range = 0
@@ -478,19 +482,43 @@ if __name__ == "__main__":
             min_range = 0
             max_range = 5000
             mesh_scale = 1.0
-            prefix = "modelnet"
+            prefix = "mcb"
 
-            file_path = paths.HOME_PATH + "../Datasets/ModelNet40/"
-            # folders = os.listdir(file_path)
-            folders = ["chair", "bench", "table", "sofa", "stool"]
+            file_path = paths.HOME_PATH + "../Datasets/MCB_A/train/"
+            folders = os.listdir(file_path)
+            # folders = ["chair", "bench", "table", "sofa", "stool"]
             num_folders = len(folders)
-            contents = paths.get_files_in_folders(base_folder=file_path, files_per_folder=max_range // num_folders, folders=folders, per_folder_subfolder="train")
+            contents = paths.get_files_in_folders(base_folder=file_path, files_per_folder=max_range // num_folders, folders=folders, per_folder_subfolder="")
             contents = contents[min_range:max_range]
             source_mesh_filenames = [file_path + file_name for file_name in contents]
     if mode == "add_augmentations" or mode == "both":
-        only_add_augmentations = False
-        add_basic_augmentations = True
-        num_extra_augmentations = 5
+        augmentation_list = []
+        # ========= Orientations ===========
+        augmentation_list.append({
+            "orientation": {"x": np.pi, "y": 0.0, "z": 0.0},
+            "scale": {"x": 1.0, "y": 1.0, "z": 1.0}
+        })
+        augmentation_list.append({
+            "orientation": {"x": np.pi / 2, "y": 0.0, "z": 0.0},
+            "scale": {"x": 1.0, "y": 1.0, "z": 1.0}
+        })
+        augmentation_list.append({
+            "orientation": {"x": -np.pi / 2, "y": 0.0, "z": 0.0},
+            "scale": {"x": 1.0, "y": 1.0, "z": 1.0}
+        })
+        # ========= Scalings ============
+        augmentation_list.append({
+            "orientation": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "scale": {"x": 2.0, "y": 1.0, "z": 1.0},
+        })
+        augmentation_list.append({
+            "orientation": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "scale": {"x": 1.0, "y": 2.0, "z": 1.0},
+        })
+        augmentation_list.append({
+            "orientation": {"x": 0.0, "y": 0.0, "z": 0.0},
+            "scale": {"x": 1.0, "y": 1.0, "z": 2.0},
+        })
     if mode == "convert_numpy":
         num_points_to_sample = 1e4
         max_mesh_per_file = 2.5e3
@@ -511,35 +539,7 @@ if __name__ == "__main__":
                               normalize_scale=normalize_scale,
                               center=normalize_center)
     if mode == "add_augmentations" or mode == "both":
-        augmentation_list = []
-        augmentation_list.append({
-            "orientation": {"x": np.pi, "y": 0.0, "z": 0.0},
-            "scale": 1
-        })
-        augmentation_list.append({
-            "orientation": {"x": np.pi/2, "y": 0.0, "z": 0.0},
-            "scale": 1
-        })
-        augmentation_list.append({
-            "orientation": {"x": -np.pi/2, "y": 0.0, "z": 0.0},
-            "scale": 1
-        })
-        # augmentation_list.append({
-        #     "orientation": {
-        #         "x": 0.0,
-        #         "y": 0.0,
-        #         "z": 0.0
-        #     },
-        #     "scale": 0.9
-        # })
-        # augmentation_list.append({
-        #     "orientation": {
-        #         "x": 0.0,
-        #         "y": 0.0,
-        #         "z": 0.0
-        #     },
-        #     "scale": 0.8
-        # })
+
 
         generate_augmentations_for_dataset(mesh_file_manager=mesh_file_manager,
                                            augmentation_list=augmentation_list,
@@ -548,7 +548,9 @@ if __name__ == "__main__":
 
     if mode == "convert_numpy":
         save_generated_dataset_as_numpy(mesh_file_manager, max_mesh_per_file, num_points_to_sample,
-                                        sampling_method=sampling_method)
+                                        sampling_method=sampling_method,
+                                        normalize_scale=normalize_scale,
+                                        center=normalize_center)
 
 
 

dataset/show_compiled_dataset.py
@@ -3,6 +3,7 @@ from dataset.process_and_save import MeshDatasetFileManager
 from heuristic_prediction.pointcloud_dataloader import PointCloudDataset
 import trimesh_util
 import pyvista as pv
+import numpy as np
 
 args = {
     # Dataset Param
@@ -16,8 +17,34 @@ args = {
 
 label_names = ["thickness"]
 
+def plot_all(c, r, dataset):
+    num_data = len(dataset)
+    num_visualized = c * r
+    num_iterations = int(np.ceil(num_data / num_visualized))
+
+    for i in range(num_iterations):
+        pl = pv.Plotter(shape=(r, c))
+        for ri in range(r):
+            for ci in range(c):
+                cloud, _, _ = dataset[i * r * c + ri * c + ci]
+                pl.subplot(ri, ci)
+                actor = pl.add_points(
+                    points=cloud,
+                    # scalars=curvature,
+                    render_points_as_spheres=True,
+                    point_size=10,
+                    show_scalar_bar=True,
+                    # text="Curvature"
+                )
+                # actor.mapper.lookup_table.cmap = 'jet'
+                pl.show_bounds(grid=True, all_edges=True,  font_size=10)
+
+
+        pl.link_views()
+        pl.show()
+
 if __name__=="__main__":
-    path = paths.DATA_PATH + "data_mcb_a/"
+    path = paths.DATA_PATH + "mcb_scale_a/"
     # file_manager = MeshDatasetFileManager(path)
     dataset = PointCloudDataset(path, args['num_points'], label_names=label_names,
                       partition='train',
@@ -27,45 +54,24 @@ if __name__=="__main__":
                       imbalance_weight_num_bins=args["imbalanced_weighting_bins"],
                       remove_outlier_ratio=args["remove_outlier_ratio"])
 
-    for i in range(len(dataset)):
-        # a = dataset[i]
-        cloud, labels, _ = dataset[i]
-        labels = labels[:, 0]
-        trimesh_util.show_sampled_values(mesh=None, points=cloud, values=labels, normalize=True)
+    all_clouds = dataset.point_clouds
+    # data x point x position
 
-def plot_all(c, r):
-    pl = pv.Plotter(shape=(1, 2))
-    stl_path = paths.HOME_PATH + "stls/low-res.stl"
-    mesh = trimesh.load(stl_path)
-    mesh_aux = trimesh_util.MeshAuxilliaryInfo(mesh)
-    points, curvature = mesh_aux.calculate_curvature_samples(count=4096)
-    pl.subplot(0, 0)
-    curvature = curvature - curvature.min(axis=0)
-    curvature /= curvature.max(axis=0)
-    actor = pl.add_points(
-        points,
-        scalars=curvature,
-        render_points_as_spheres=True,
-        point_size=10,
-        show_scalar_bar=True,
-        # text="Curvature"
-    )
-    pl.add_text('Curvature', color='w')
-    actor.mapper.lookup_table.cmap = 'jet'
 
-    points, thickness = mesh_aux.calculate_thicknesses_samples(count=4096)
-    pl.subplot(0, 1)
-    thickness = thickness - thickness.min(axis=0)
-    thickness /= thickness.max(axis=0)
-    actor = pl.add_points(
-        points,
-        scalars=thickness,
-        render_points_as_spheres=True,
-        point_size=10,
-        show_scalar_bar=True,
-    )
-    pl.add_text('Thickness', color='w')
-    actor.mapper.lookup_table.cmap = 'jet'
+    print("avg_position: ", np.mean(np.mean(dataset.point_clouds, axis=1), axis=0))
+    print("min_position: ", np.min(np.min(dataset.point_clouds, axis=1), axis=0))
+    print("max_position: ", np.max(np.max(dataset.point_clouds, axis=1), axis=0))
+    lengths = np.max(dataset.point_clouds, axis=1) - np.min(dataset.point_clouds, axis=1)
 
-    pl.link_views()
-    pl.show()
+    print("avg_length: ", np.mean(lengths, axis=0))
+    print("min_length: ", np.min(lengths, axis=0))
+    print("max_length: ", np.max(lengths, axis=0))
+
+    plot_all(5, 6, dataset)
+    # for i in range(len(dataset)):
+    #     # a = dataset[i]
+    #     cloud, labels, _ = dataset[i]
+    #     labels = labels[:, 0]
+    #     trimesh_util.show_sampled_values(mesh=None, points=cloud, values=labels, normalize=True)
+
+

heuristic_prediction/dgcnn_train.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import torch
 import torch.optim as optim
-from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts
+from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts, ReduceLROnPlateau
 from heuristic_prediction.regression_tools import RegressionTools, succinct_label_save_name, seed_all
 import paths
 from heuristic_prediction.dgcnn_model import DGCNN_param, DGCNN_segment
@@ -29,9 +29,10 @@ model_args = {
 experiment_name = succinct_label_save_name(label_names)
 
 args = {
-    "dataset_name": "prim_cone_hole",
+    "dataset_name": "mcb_scale_a",
     "exp_name": experiment_name,
     "label_names": label_names,
+    "seed": 1,
 
     # Dataset Param
     "data_fraction": 1.0,
@@ -39,10 +40,8 @@ args = {
     "do_test": True,
     "workers": 24,
 
-    "normalize_inputs": False,
     "sampling_method": "even",
     "imbalanced_weighting_bins": 1, #1 means no weighting
-
     "normalize_outputs": False,
     "remove_outlier_ratio": 0.1, # 0 means remove no outliers
 
@@ -50,13 +49,15 @@ args = {
     "batch_size": 16,
     "test_batch_size": 32,
     "grad_acc_steps": 2,
-    "epochs": 50,
-    "lr": 1e-2,
-    "min_lr": 5e-4,
+    "epochs": 100,
+    "lr": 1e-3,
     "weight_decay": 1e-5,
-    "scheduler": "none",
-    "seed": 1,
-    "restarts": 3,
+
+    "scheduler": "plateau",
+    # "restarts": 3,
+    # "min_lr": 5e-4,
+    "patience": 5,
+    "factor": 0.1
 }
 
 args.update(model_args)
@@ -69,7 +70,7 @@ if __name__ == "__main__":
                                                 partition='train',
                                                 # filter_criteria=filter_criteria, use_augmentations=True,
                                                 data_fraction=args["data_fraction"], use_numpy=True,
-                                                normalize=args["normalize_inputs"],
+                                                # normalize=args["normalize_inputs"],
                                                 normalize_outputs=args["normalize_outputs"],
                                                 sampling_method=args["sampling_method"],
                                                 outputs_at=args["outputs_at"],
@@ -81,7 +82,7 @@ if __name__ == "__main__":
                                                partition='test',
                                                # filter_criteria=filter_criteria, use_augmentations=False,
                                                data_fraction=args["data_fraction_test"], use_numpy=True,
-                                               normalize=args["normalize_inputs"],
+                                               # normalize=args["normalize_inputs"],
                                                normalize_outputs=False,
                                                sampling_method=args["sampling_method"],
                                                outputs_at=args["outputs_at"],
@@ -103,8 +104,8 @@ if __name__ == "__main__":
             scheduler = CosineAnnealingLR(opt, args["epochs"], eta_min=args["min_lr"])
         else:
             scheduler = CosineAnnealingWarmRestarts(opt, args["epochs"], T_mult=args["restarts"], eta_min=args["min_lr"])
-
-
+    if args["scheduler"] == "plateau":
+            scheduler = ReduceLROnPlateau(opt, patience=args["patience"], factor=args["factor"])
 
 
     regression_manager = RegressionTools(

heuristic_prediction/pointcloud_dataloader.py
@@ -2,6 +2,8 @@ import numpy as np
 import trimesh
 from torch.utils.data import Dataset
 from tqdm import tqdm
+
+import util
 from dataset.process_and_save import MeshDatasetFileManager, get_augmented_mesh
 from dataset.imbalanced_data import get_imbalanced_weight_nd, non_outlier_indices, non_outlier_indices_vertices_nclass
 
@@ -61,18 +63,18 @@ def load_point_clouds_manual(data_root_dir, num_points, label_names, filter_crit
 
 class PointCloudDataset(Dataset):
     def __init__(self, data_root_dir, num_points, label_names, partition='train', filter_criteria=None, outputs_at="global",
-                 use_augmentations=True, data_fraction=1.0, use_numpy=True, normalize=False, sampling_method="mixed",
+                 use_augmentations=True, data_fraction=1.0, use_numpy=True, sampling_method="mixed",
                  imbalance_weight_num_bins=1, normalize_outputs=False, remove_outlier_ratio=0.05):
         # filter_criteria of the form filter_criteria(mesh, instance_data)->boolean
         self.outputs_at = outputs_at
+        timer = util.Stopwatch()
+        timer.start()
         if use_numpy:
             print("Loading data from numpy...")
             if use_augmentations:
                 print("Warning: augmentations are not being used")
             if filter_criteria is not None:
-                    print("Warning: filters are not being used")
-            if normalize:
-                print("Note: Data is being normalized")
+                print("Warning: filters are not being used")
             self.point_clouds, self.label = load_point_clouds_numpy(data_root_dir=data_root_dir,
                                                                     num_points=2 * num_points,
                                                                     label_names=label_names,
@@ -90,28 +92,28 @@ class PointCloudDataset(Dataset):
                                                                      use_augmentations=use_augmentations,
                                                                      sampling_method=sampling_method)
         # Normalize each target
-        if normalize:
-            print("Warning: should not set normalize flag of PointCloudDataset anymore")
-            # Normalize inputs
-            # First get bounding boxes
-            bound_max = np.max(self.point_clouds, axis=1)
-            bound_min = np.min(self.point_clouds, axis=1)
-            centroid = np.mean(self.point_clouds, axis=1)
-            bound_length = np.max(bound_max - bound_min, axis=1) # maximum length
-            # scale to box of 0, 1
-            self.normalization_scale = 1.0/bound_length
-            normalization_scale_multiplier = np.repeat(self.normalization_scale[:, np.newaxis], len(self.point_clouds[0]), axis=1)
-            normalization_scale_multiplier = np.repeat(normalization_scale_multiplier[:, :, np.newaxis], 3, axis=2)
-
-            centering = np.repeat(centroid[:, np.newaxis, :], num_points*2, axis=1)
-            self.point_clouds -= centering
-            self.point_clouds = normalization_scale_multiplier * self.point_clouds
-
-            self.normalization_order = 3
-            self.label = self.label * np.repeat(np.power(self.normalization_scale, self.normalization_order)[:, np.newaxis], len(self.label[0]), axis=1)
+        # if normalize:
+        #     print("Warning: should not set normalize flag of PointCloudDataset anymore")
+            # # Normalize inputs
+            # # First get bounding boxes
+            # bound_max = np.max(self.point_clouds, axis=1)
+            # bound_min = np.min(self.point_clouds, axis=1)
+            # centroid = np.mean(self.point_clouds, axis=1)
+            # bound_length = np.max(bound_max - bound_min, axis=1) # maximum length
+            # # scale to box of 0, 1
+            # self.normalization_scale = 1.0/bound_length
+            # normalization_scale_multiplier = np.repeat(self.normalization_scale[:, np.newaxis], len(self.point_clouds[0]), axis=1)
+            # normalization_scale_multiplier = np.repeat(normalization_scale_multiplier[:, :, np.newaxis], 3, axis=2)
+            #
+            # centering = np.repeat(centroid[:, np.newaxis, :], num_points*2, axis=1)
+            # self.point_clouds -= centering
+            # self.point_clouds = normalization_scale_multiplier * self.point_clouds
+            #
+            # self.normalization_order = 3
+            # self.label = self.label * np.repeat(np.power(self.normalization_scale, self.normalization_order)[:, np.newaxis], len(self.label[0]), axis=1)
 
         if normalize_outputs:
-            print("Warning: should not set normalize_outputs flag of PointCloudDataset anymore")
+            print("Normalizing Outputs")
             length = np.max(self.label, axis=0) - np.min(self.label, axis=0)
             center = np.mean(self.label, axis=0)
             self.label = (self.label - center) / length
@@ -134,6 +136,7 @@ class PointCloudDataset(Dataset):
             self.weights = get_imbalanced_weight_nd(self.label, num_bins=imbalance_weight_num_bins, modifier=None)
             # self.weights = np.sqrt(self.weights)
 
+        print("Time to load data: ", timer.get_time())
         self.num_points = num_points
         self.partition = partition
 

heuristic_prediction/regression_tools.py
@@ -8,7 +8,7 @@ import os
 from pathlib import Path
 import json
 import util
-from util import IOStream
+from util import IOStream, Stopwatch
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -43,6 +43,7 @@ class RegressionTools:
         print("Saving checkpoints to: ", self.checkpoint_path)
         self.io = self.create_checkpoints(args)
         self.gradient_accumulation_steps = args["grad_acc_steps"]
+        # self.io.cprint("Total training data: " + str(self.train_loader.))
 
 
     def create_checkpoints(self, args):
@@ -96,7 +97,10 @@ class RegressionTools:
                     train_pred.append(preds.detach().cpu().numpy())
 
             if self.scheduler is not None:
-                self.scheduler.step()
+                if self.args["scheduler"] == "plateau":
+                    self.scheduler.step(loss)
+                else:
+                    self.scheduler.step()
 
             train_true = np.concatenate(train_true)
             train_pred = np.concatenate(train_pred)
@@ -118,7 +122,11 @@ class RegressionTools:
                       '\n\tr2: %s '
                       '\n\tlr: %s '
                       '\n\tacc: %s' %
-                      (epoch, train_loss * 1.0 / count, str(res), self.opt.param_groups[0]['lr'], str(acc)))
+                      (epoch,
+                       train_loss * 1.0 / count,
+                       str(res),
+                       self.opt.param_groups[0]['lr'],
+                       str(acc)))
             self.io.cprint(outstr)
 
             loss_history.append(train_loss * 1.0 / count)
@@ -126,6 +134,8 @@ class RegressionTools:
 
             # Loss
             if plot_every_n_epoch >= 1 and epoch % plot_every_n_epoch == 0:
+                timer = Stopwatch()
+                timer.start()
                 plt.figure(0)
                 plt.clf()
                 for i in range(len(res)):
@@ -155,6 +165,7 @@ class RegressionTools:
                 plt.ylabel("Loss")
                 plt.xlabel("Epoch")
                 plt.savefig(self.checkpoint_path + "images/" + 'loss.png')
+                print("Time to save figures: ", timer.get_time())
 
 
 